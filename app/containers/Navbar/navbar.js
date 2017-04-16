5e6d24f6104a168c4eecbde56cca4e55ba113f45
John Son <jsson77@gmail.com>
2017-04-15 18:37:26 -0700

    router setup

--- app/containers/Navbar/navbar.js
+++ app/containers/Navbar/navbar.js
@@ -1,1 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+export default class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+  // static propTypes = {
+  //   children: React.PropTypes.node,
+  // };
+
+  render() {
+    return (
+      <div>
+        {React.Children.toArray(this.props.children)}
+      </div>
+    );
+  }
+}
